fba93770bd7c9de05c8e581de9299b5dfa520398
turbolek <dankunicki@gmail.com>
2022-09-01 13:53:11 +0200

    Cancelling movement tasks

--- Cavemen/Assets/Unit.cs
+++ Cavemen/Assets/Unit.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -8,8 +9,14 @@ public class Unit : MonoBehaviour
     [SerializeField]
     private float _movementSpeed;
 
+    private CancellationTokenSource _moveCancelationToken;
 
     public bool IsSelected { get; private set; } = false;
+
+    private void Start()
+    {
+
+    }
 
     public void Select()
     {
@@ -25,10 +32,16 @@ public class Unit : MonoBehaviour
 
     public void Move(Vector3 groundPosition)
     {
-        MoveTask(groundPosition);
+        if (_moveCancelationToken != null)
+        {
+            _moveCancelationToken.Cancel();
+        }
+
+        _moveCancelationToken = new CancellationTokenSource();
+        MoveTask(groundPosition, _moveCancelationToken.Token);
     }
 
-    private async Task MoveTask(Vector3 groundPosition)
+    private async Task MoveTask(Vector3 groundPosition, CancellationToken cancellationToken)
     {
         Vector3 targetPosition = new Vector3(groundPosition.x, transform.position.y, groundPosition.z);
         transform.LookAt(targetPosition, Vector3.up);
@@ -37,7 +50,7 @@ public class Unit : MonoBehaviour
         float stepDistance = _movementSpeed * Time.deltaTime;
         Vector3 direction = (targetPosition - transform.position).normalized;
 
-        while (stepDistance < distance)
+        while (stepDistance < distance && !cancellationToken.IsCancellationRequested)
         {
             transform.position = transform.position + direction * stepDistance;
             distance = Vector3.Distance(targetPosition, transform.position);
@@ -46,6 +59,9 @@ public class Unit : MonoBehaviour
             await Task.Yield();
         }
 
-        transform.position = targetPosition;
+        if (!cancellationToken.IsCancellationRequested)
+        {
+            transform.position = targetPosition;
+        }
     }
 }
